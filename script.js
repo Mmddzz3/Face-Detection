23015a3c7d14eafe6bd40f4fbaf769dfe6f292de
Madhura P <madhura.pharande@somaiya.edu>
2020-06-15 12:56:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const video = document.getElementById('video');
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromURi('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromURi('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromURi('/models'),
+    faceapi.nets.faceExpressionNetloadFromURi('/models'),
+]).then(startVideo)
+
 function startVideo() {
     navigator.getUserMedia(
         { video: {} },
@@ -8,4 +15,3 @@ function startVideo() {
     )
 }
 
-startVideo();
